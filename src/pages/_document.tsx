ff4ea7e6a4de7810517119fe8f79f15c295115d1
Juliu Cesar <juliucesarcardoso@gmail.com>
2023-04-03 20:18:38 -0300

    testing tailwind

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="pt-br">
       <Head />
-      <body>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Kanit:wght@300;400;500;600;700&display=swap"
+        rel="stylesheet"
+      />
+      <body className="bg-gradient-to-r from-[#303030] to-[#303540]">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
